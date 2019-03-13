d4e1829489820888a9f8ce084d075cf912668225
CelsoEspinoza <c.espinoza.arce@outlook.com>
2019-03-13 11:02:50 -0500

    feat(MIG): Quitando code de más

--- src/api/plugins-register.js
+++ src/api/plugins-register.js
@@ -1,17 +1,7 @@
 "use strict";
 
 const pagiJapi = require("./shared/paginate");
-// const notificationTypePlugin = require("./notification-types/notification-types.plugin");
-// const notificationPlugin = require("./notifications/notifications.plugin");
-// const templatePlugin = require("./templates/templates.plugin");
-// const blackListPlugin = require("./black-list/black-list.plugin");
 
-const plugins = [
-  pagiJapi
-  // notificationTypePlugin,
-  // notificationPlugin,
-  // templatePlugin,
-  // blackListPlugin
-];
+const plugins = [pagiJapi];
 
 module.exports = plugins;
