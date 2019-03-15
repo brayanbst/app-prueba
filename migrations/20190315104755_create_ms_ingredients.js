e91d5debd64230a3a8a0c10609cc5ffba0120fce
brayan <sagitario202008>
2019-03-15 15:54:40 -0500

    feat(MIG): AGREGANDO DECIMALES

--- migrations/20190315104755_create_ms_ingredients.js
+++ migrations/20190315104755_create_ms_ingredients.js
@@ -6,7 +6,7 @@ exports.up = knex =>
 	knex.schema.createTable('ms_ingredients', (table) => {
 		table.increments().primary();
 		table.text('name').nullable();
-		table.decimal('energy_value').nullable();
+		table.decimal('energy_value', 18, 2).nullable();
 		table.decimal('carbs_value', 18, 2).nullable();
 		table.decimal('lips_value', 18, 2).nullable();
 		table.decimal('protein_value', 18, 2).nullable();
