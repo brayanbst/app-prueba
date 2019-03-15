bfc0c423221c202820743ee8c024420072d45463
brayan <sagitario202008>
2019-03-15 09:53:58 -0500

    feat(MIG): CREANDO MIGRACION DE CLASES

---
+++ migrations/20190315092503_create_ms_class.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const helper = require('./../src/shared/helperMigration');
+
+exports.up = knex =>
+	knex.schema.createTable('ms_class', (table) => {
+		table.increments().primary();
+		table.text('url_video').nullable();
+		table.text('name').nullable();
+		table.text('description').nullable();
+		table.integer('ms_trainers').unsigned();
+		table.foreign('ms_trainers').references('ms_trainers.id');
+		table.integer('ms_sections').unsigned();
+		table.foreign('ms_sections').references('ms_sections.id');
+		helper.defaultColumns(table, false);
+	});
+
+exports.down = knex =>
+	knex.schema.dropTable('ms_class');
