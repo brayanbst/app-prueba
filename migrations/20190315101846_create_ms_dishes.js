f054e960639eeaf41df76baa91571cca16a69b91
brayan <sagitario202008>
2019-03-15 15:47:08 -0500

    feat(MIG) : CREANDO LA MIGRACION DE LA TABLA PLATOS

--- migrations/20190315101846_create_ms_dishes.js
+++ migrations/20190315101846_create_ms_dishes.js
@@ -8,7 +8,7 @@ exports.up = knex =>
 		table.text('name').nullable();
 		table.text('description').nullable();
 		table.boolean('is_favorite').nullable();
-		table.boolean('section_id').unsigned().nullable();
+		table.integer('section_id').unsigned().nullable();
 		table.foreign('section_id').references('ms_sections.id');
 		helper.defaultColumns(table, false);
 	});
